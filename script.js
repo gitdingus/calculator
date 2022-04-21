5bfb688194be74efb3e5d142967ee81122abe8fd
Josh S <aaabccc87@protonmail.com>
2022-04-21 14:10:26 -0400

    Calculator semi operational

--- script.js
+++ script.js
@@ -2,9 +2,94 @@ const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
 
 Array.from(buttons).forEach(button => button.addEventListener('click', buttonClick));
+let fillFirst = true;
+let addPeriod = true;
+let firstNumStr = "";
+let secondNumStr = "";
+let operationStr = "";
+let result;
 
 function buttonClick(e){
-    console.log(`Value of button clicked: ${this.value}`);
+    if (this.classList.contains("number")){
+        fillNumber(this.value);
+
+        if (fillFirst){
+            display.textContent = firstNumStr;
+        }
+        else {
+            display.textContent = secondNumStr;
+        }
+    }
+    else if (this.classList.contains("period")){
+        if (addPeriod){
+            fillNumber('.');
+        }
+
+        addPeriod = false;
+    }
+    else if (this.classList.contains("operator")){
+        fillOperator(this.value);
+        fillFirst = false;
+        addPeriod = true;
+    }
+    else if (this.classList.contains("equals")){
+        if (firstNumStr && secondNumStr && operationStr){
+            let x = Number(firstNumStr);
+            let y = Number(secondNumStr);
+            result = operate(operationStr, x, y);
+
+            display.textContent = result;
+        }
+
+        firstNumStr = '';
+        secondNumStr = '';
+        operationStr = '';
+
+        fillFirst = true;
+        addPeriod = true;
+    }
+    else if (this.classList.contains("clear")){
+        clearButtonClicked(this.value);
+    }
+}
+
+function clearButtonClicked(value){
+
+    if (value === "AC"){
+        firstNumStr = '';
+        secondNumStr = '';
+        operator = '';
+        display.textContent = '';
+    }
+
+}
+
+function fillNumber(numChar){
+    if (fillFirst){
+        firstNumStr += numChar;
+    }
+    else {
+        secondNumStr += numChar;
+    }
+}
+
+function fillOperator(opValue){
+    switch (opValue){
+        case 'add':
+            operationStr = '+';
+            break;
+        case 'subtract':
+            operationStr = '-';
+            break;
+        case 'multiply':
+            operationStr = '*';
+            break;
+        case 'divide':
+            operationStr = '/';
+            break;
+        default:
+            return 'INVALID_OPERATOR';
+    }
 }
 
 function add(x, y){
